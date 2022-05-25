4d5f50d60c51b0072eef9701bea89fc4a164e095
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-25 15:28:03 +0100

    readme

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 import javax.inject._
 import scala.concurrent.Future
 
-
+//
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
