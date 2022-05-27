c02d6a6f15c9d3774876226c3f57cd80f9095f3f
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-27 10:23:50 +0100

    creating data access layer

---
+++ app/models/DataModel.scala
@@ -0,0 +1,15 @@
+package models
+
+import play.api.libs.json.{Json, OFormat}
+
+case class DataModel(_id: String,
+                       name: String,
+                       description: String,
+                       numSales: Int){
+
+  val bookOne: DataModel = DataModel("id1", "Frankenstein", "Mary Shelley", 2000)
+}
+
+object DataModel {
+  implicit val formats: OFormat[DataModel] = Json.format[DataModel]
+}
--- app/models/DataModels/scala.scala
+++
@@ -1,5 +0,0 @@
-package models.DataModels
-
-class scala {
-
-}
