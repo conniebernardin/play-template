8305183ea2c76b5e6d45496351d4a777b4a5b22b
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-27 10:15:53 +0100

    test driven development

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,15 +1,22 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers.{defaultAwaitTimeout, status}
 
 class ApplicationControllerSpec extends BaseSpecWithApplication{
 
   val TestApplicationController = new ApplicationController(
-    component,
-    repository)
+    component)
 
-  "ApplicationController .index()" should {
+  "ApplicationController .index" should {
 
+    val result = TestApplicationController.index()(FakeRequest())
+
+    "return TODO" in {
+      status(result) shouldBe Status.NOT_IMPLEMENTED
+    }
   }
 
   "ApplicationController .create()" should {
