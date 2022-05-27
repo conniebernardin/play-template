8c065c57c2fc2536d3aa2fb69302a7fb8e358d42
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-27 10:26:49 +0100

    creating data model

--- app/models/DataModel.scala
+++ app/models/DataModel.scala
@@ -3,9 +3,9 @@ package models
 import play.api.libs.json.{Json, OFormat}
 
 case class DataModel(_id: String,
-                       name: String,
-                       description: String,
-                       numSales: Int){
+                     name: String,
+                     description: String,
+                     numSales: Int){
 
   val bookOne: DataModel = DataModel("id1", "Frankenstein", "Mary Shelley", 2000)
 }
