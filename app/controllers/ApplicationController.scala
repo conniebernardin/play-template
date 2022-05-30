78e69709dd91e3581fcbe26c2cbedba87b64a083
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-30 08:39:39 +0100

    implement index method

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,11 +1,19 @@
 package controllers
+import models.DataModel
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import repositories.DataRepository
+
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController{
 
-  def index() = TODO
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    val books: Future[Seq[DataModel]] = dataRepository.collection.find().toFuture()
+    books.map(items => Json.toJson(items)).map(result => Ok(result))
+  }
 
   def create() = TODO
   def read(id: String) = TODO
