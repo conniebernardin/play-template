99a9b2b1d78e9ba5599f2e157b420b96e476005c
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-30 14:47:26 +0100

    update and create initial methods

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,36 +1,44 @@
 package controllers
 import models.DataModel
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import repositories.DataRepository
 
-import java.awt.print.Book
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController{
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository, implicit val ec: ExecutionContext) extends BaseController {
 
   def index(): Action[AnyContent] = Action.async { implicit request =>
     val books: Future[Seq[DataModel]] = dataRepository.collection.find().toFuture()
     books.map(items => Json.toJson(items)).map(result => Ok(result))
   }
 
-  def create(book: DataModel): Action[AnyContent] = Action { implicit request =>
-    dataRepository.create(book)
-    Ok
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.create(dataModel).map(_ => Created)
+      case JsError(_) => Future(BadRequest)
+    }
   }
 
   def read(id: String): Action[AnyContent] = Action.async { implicit request =>
-  val book = dataRepository.read(id)
-  book.map(items => Json. toJson(items)).map(result => Ok(result))
+    val book = dataRepository.read(id)
+    book.map(items => Json.toJson(items)).map(result => Ok(result))
   }
 
-  def update(id: String) = TODO
-
-  def delete(id: String): Action[AnyContent] = Action { implicit request =>
-    dataRepository.delete(id)
-    Accepted
+  def update(id: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.update(id, dataModel).map(result => Accepted(result))
+      case JsError(_) => Future(BadRequest)
+    }
   }
+
+    def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+      dataRepository.delete(id)
+      Future(Accepted)
+    }
 
 }
