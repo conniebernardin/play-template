2c13d4f1dcd1983a7f14d34d86e320d021f1d064
Connie Bernardin <conniebernardin@googlemail.com>
2022-06-08 12:07:47 +0100

    adding read method test

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -6,7 +6,7 @@ import play.api.test.FakeRequest
 import play.api.http.Status
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.Result
-import play.api.test.Helpers.{defaultAwaitTimeout, status}
+import play.api.test.Helpers.{contentAsJson, defaultAwaitTimeout, status}
 
 import scala.concurrent.Future
 import scala.reflect.internal.NoPhase
@@ -46,7 +46,23 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
     }
   }
 
+
   "ApplicationController .read()" should {
+    beforeEach()
+    "find a book in the database by id" in {
+
+      val request: FakeRequest[JsValue] = buildPost("/api/create").withBody[JsValue](Json.toJson(dataModel))
+
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+     
+      val readResult: Future[Result] = TestApplicationController.read("abcd")(FakeRequest()) // works without having readRequest
+
+      status(createdResult) shouldBe Status.CREATED
+      status(readResult) shouldBe Status.OK
+      contentAsJson(readResult).as[DataModel] shouldBe DataModel("abcd", "test name", "test description", 100)
+
+    }
+    afterEach()
   }
 
   "ApplicationController .update()" should {
